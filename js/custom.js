52846a4719e8da737e2b1fadeb6361c14ad2747b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-10 12:50:00 -0800

    Added team section. HTML, CSS, & Javascript

--- js/custom.js
+++ js/custom.js
@@ -22,3 +22,19 @@ $(function () {
 
    });
 });
+
+/* ==============================================
+|   |   |   |    Team
+=============================================== */
+
+$(function () {
+
+   $(".team-members").owlCarousel({
+       items: 3,
+       autoplay: true,
+       smartSpeed: 700,
+       loop: true,
+       autoplayHoverPause: true
+
+   });
+});
