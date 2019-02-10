545f802155c2f87bee81a6632469f09c65e691f2
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-10 11:28:35 -0800

    Added work section. HTML, CSS & Javascript

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,22 @@
 =============================================== */
 $(function () {
 
+    // animate on scroll
     new WOW().init();
 });
 
+
+/* ==============================================
+|   |   |   |    Work
+=============================================== */
+$(function () {
+
+   $("#work").magnificPopup({
+       delegate: 'a',
+       type: 'image',
+       gallery: {
+           enabled: true
+       }
+
+   });
+});
