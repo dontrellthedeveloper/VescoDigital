a28cadb20205dcfd43090cc90ae74a55ee47f90f
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-10 14:01:53 -0800

    Added testimonials section. HTML, CSS, & Javascript

--- js/custom.js
+++ js/custom.js
@@ -38,3 +38,19 @@ $(function () {
 
    });
 });
+
+/* ==============================================
+|   |   |   |    Team
+=============================================== */
+
+$(function () {
+
+    $(".customer-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+
+    });
+});
