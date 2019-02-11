95d400f7594492b64dbf06f83b4563b653a8f49a
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2019-02-10 20:40:51 -0800

    Added navigation. HTML, CSS & Javascript. Also added smooth scroll & Back to top button

--- js/custom.js
+++ js/custom.js
@@ -120,3 +120,40 @@ $(window).on('load', function () {
 
 
 });
+
+
+
+/* ================================
+|   |   |   Navigation
+================================ */
+
+$(function () {
+
+   $(window).scroll(function () {
+
+       if($(this).scrollTop() < 50 ) {
+           $("nav").removeClass("vesco-top-nav");
+           $("#back-to-top").fadeOut();
+       } else {
+           $("nav").addClass("vesco-top-nav");
+           $("#back-to-top").fadeIn();
+       }
+   });
+
+});
+
+// Smooth Scrolling
+
+$(function () {
+
+    $("a.smooth-scroll").click(function(event) {
+
+       event.preventDefault();
+
+       var section = $(this).attr("href");
+
+       $("html, body").animate({
+          scrollTop: $(section).offset().top - 64
+       }, 1250, "easeInOutExpo");
+    });
+});
